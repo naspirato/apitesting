909a0bfc6e395946bf04f89575c2e2c98d8e2bd4
dmilinevich <dm@zvuk.com>
2018-10-03 23:31:49 +0300

    try to fix cucumber report...need some more working on it

--- apitesting/src/test/java/com/zvuk/ParallelZvukRun.java
+++ apitesting/src/test/java/com/zvuk/ParallelZvukRun.java
@@ -1,4 +1,4 @@
-package examples;
+package com.zvuk;
 import com.intuit.karate.cucumber.CucumberRunner;
 import com.intuit.karate.cucumber.KarateStats;
 import cucumber.api.CucumberOptions;
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 @CucumberOptions(tags = {"~@ignore"})
-public class ZvukTestParallel {
+public class ParallelZvukRun {
 
   /*  @Test
     public void testParallel() {
--- apitesting/src/test/java/com/zvuk/ZvukTestRunner.java
+++ apitesting/src/test/java/com/zvuk/ZvukTestRunner.java
@@ -1,11 +1,10 @@
-package examples;
+package com.zvuk;
 
 import com.intuit.karate.junit4.Karate;
-import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
-public class TestRunner {
+public class ZvukTestRunner {
     // this will run all *.feature files that exist in sub-directories
     // refer to https://github.com/intuit/karate#naming-conventions
     // for folder-structure recommendations and naming conventions
